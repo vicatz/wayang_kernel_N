aabd756f9259eb02cd5d24d28c8eb4d4ee631a60
Albert I <krascgq@outlook.co.id>
2017-09-10 00:21:42 +0800

    kernel: Eliminate misleading-indentation warnings
    Change-Id: Ie8bd4cb04c1578ba09ac296b2c09bc4bc5825777
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/video/msm/mdss/msm_mdss_io_8974.c
+++ drivers/video/msm/mdss/msm_mdss_io_8974.c
@@ -1101,19 +1101,19 @@ static void mdss_dsi_phy_regulator_ctrl(struct mdss_dsi_ctrl_pdata *ctrl,
 			mdss_dsi_8996_phy_regulator_enable(ctrl);
 		} else {
 			switch (ctrl->shared_data->hw_rev) {
-			case MDSS_DSI_HW_REV_103:
-				mdss_dsi_20nm_phy_regulator_enable(ctrl);
-				break;
-			default:
-			/*
-			 * For dual dsi case, do not reconfigure dsi phy
-			 * regulator if the other dsi controller is still
-			 * active.
-			 */
-			if (!mdss_dsi_is_hw_config_dual(sdata) ||
-				(other_ctrl && (!other_ctrl->is_phyreg_enabled
-						|| other_ctrl->mmss_clamp)))
-				mdss_dsi_28nm_phy_regulator_enable(ctrl);
+				case MDSS_DSI_HW_REV_103:
+					mdss_dsi_20nm_phy_regulator_enable(ctrl);
+					break;
+				default:
+				/*
+				 * For dual dsi case, do not reconfigure dsi phy
+				 * regulator if the other dsi controller is still
+				 * active.
+				 */
+				if (!mdss_dsi_is_hw_config_dual(sdata) ||
+					(other_ctrl && (!other_ctrl->is_phyreg_enabled
+							|| other_ctrl->mmss_clamp)))
+					mdss_dsi_28nm_phy_regulator_enable(ctrl);
 				break;
 			}
 		}
