4220de331d61297b30d008c6603b5783feec882d
viky dwi santoso <vickky@programmer.net>
2018-03-22 23:40:17 +0700

    import build script

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+kernel_dir=$PWD
+export V="v1.4"
+export CONFIG_FILE="mido_defconfig"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=vicatz
+export KBUILD_BUILD_HOST=Dalang
+export TOOL_CHAIN_PATH="/home/vicatz/toolchains/ubertc_gcc/aarch64-linux-android-8.0/bin/aarch64-linux-android-"
+export CLANG_TCHAIN="/home/vicatz/toolchains/alize-clang/clang-4536805/bin/clang"
+export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | cut -d'(' -f1,4)"
+export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
+export PATH=$PATH:${TOOL_CHAIN_PATH}
+export builddir="${kernel_dir}/mateng"
+#export modules_dir="zip/system/lib/modules"
+export ZIPPER_DIR="${kernel_dir}/zip"
+export ZIP_NAME="wayang™${V}_Nougat-Los_Aosp.zip"
+export IMAGE="arch/arm64/boot/Image.gz-dtb";
+JOBS="-j8"
+cd $kernel_dir
+make clean && make mrproper
+
+make_a_fucking_defconfig() {
+	make $CONFIG_FILE
+}
+
+compile() {
+	PATH=${BIN_FOLDER}:${PATH} make \
+	O=${out_dir} \
+	CC="${CLANG_TCHAIN}" \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=${TOOL_CHAIN_PATH} \
+    KBUILD_COMPILER_STRING="${CLANG_VERSION}" \
+	HOSTCC="${CLANG_TCHAIN}" \
+    $JOBS
+}
+
+zipit () {
+    if [[ ! -f "${IMAGE}" ]]; then
+        echo -e "Build failed :P";
+        exit 1;
+    else
+        echo -e "Build Succesful!";
+    fi
+    echo "**** Copying zImage ****"
+    cp arch/arm64/boot/Image.gz-dtb ${ZIPPER_DIR}/tools/
+    echo "**** Copying modules ****"
+
+    cd ${ZIPPER_DIR}/
+    zip -r9 ${ZIP_NAME} * -x README ${ZIP_NAME}
+    rm -rf ${kernel_dir}/mateng/${ZIP_NAME}
+    mv ${ZIPPER_DIR}/${ZIP_NAME} ${kernel_dir}/mateng/${ZIP_NAME}
+}
+
+make_a_fucking_defconfig
+compile
+zipit
+cd ${kernel_dir}
